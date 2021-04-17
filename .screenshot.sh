77362a9dfeb8b6828b406e2f24969afda12c882a
Beyley Thomas <ep1cm1n10n123@gmail.com>
2021-04-17 02:13:27 +0000

    Add files via upload

--- .screenshot.sh
+++ .screenshot.sh
@@ -26,14 +26,26 @@ gio mount $server/$share < $credfile
 
 # Create the temporary directory
 mkdir $tempfolder
+
+echo "Server type is $conntype"
+
+if [ $conntype == 'smb' ] 
+then
+	mount_folder=smb-share\:server\=$server_name\,share\=$share
+elif [ $conntype == 'ftp' ]
+then
+	mount_folder=ftp\:host\=$server_name
+fi
+
 # Links the mounted dir to the temporary one
-ln -s /run/user/1000/gvfs/smb-share\:server\=$server_name\,share\=$share/ $tempfolder/
+echo "ln -s /run/user/1000/gvfs/$mount_folder/ $tempfolder/"
+ln -s /run/user/1000/gvfs/$mount_folder/ $tempfolder/
 
 # Generates a random string
 random=$(xxd -l $random_string_length -c $random_string_length -p < /dev/urandom)
 
 # Copy the temporary image to the samba share
-cp /tmp/temp.png $tempfolder/smb-share\:server\=$server_name\,share\=$share/$path_to_move_to/$random.png
+cp /tmp/temp.png $tempfolder/$mount_folder/$path_to_move_to/$random.png
 
 # Remove the temporary image from the temp folder
 rm /tmp/temp.png
